7fb959c47712e3ec41bec335167dc144ed121bb1
Kim HyunWoo <stories2@naver.com>
2020-05-27 21:02:13 +0900

    body 에 태그 생성 및 드래드 드랍 이벤트 활성

---
+++ index.js
@@ -0,0 +1,37 @@
+javascript: (function () {
+
+    $("body").append(`
+        <div id='dropbox' style='
+        position: absolute;
+        left: calc(50% - 150px);
+        bottom: 100px;
+        width: 300px;
+        height: 150px;
+        z-index: 9999999;
+        background-color: #20639B;
+        border-radius: 25px;
+        padding: 15px;
+        opacity: 0.9;
+        color: white;
+        text-align: center;
+        align-items: center;
+        line-height: 105px;
+        '>Drop it</div>`);
+    
+    $("#dropbox").on("dragover", (ev) => {
+        ev.preventDefault();
+        console.log('dragover-event', ev);
+    });
+
+    $("#dropbox").on("drop", (ev) => {
+        ev.preventDefault();
+        console.log('drop-event', ev);
+        if (ev.originalEvent.dataTransfer.files && ev.originalEvent.dataTransfer.files.length > 0) {
+            const file = ev.originalEvent.dataTransfer.files[0];
+            console.log('file', file);
+          }
+    });
+ }());
+
+
+
