86740faa8ea83e7f1ff61b021d83d17c86e0a85b
Kim HyunWoo <stories2@naver.com>
2020-05-27 22:35:37 +0900

    ascii art 내용 구름으로 작성

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ javascript: (function () {
         return grayScales;
     };
 
-    const MAXIMUM_WIDTH = 50;
-    const MAXIMUM_HEIGHT = 50;
+    const MAXIMUM_WIDTH = 100;
+    const MAXIMUM_HEIGHT = 100;
 
     const clampDimensions = (width, height) => {
         if (height > width) {
@@ -85,8 +85,50 @@ javascript: (function () {
         return ascii;
     };
 
-    const makeCloud = (videoID, videoTime, hash) => {
+    const reqCloud = (data) => new Promise((resolve, reject) => {
+        $.ajax({  
+            type: "POST" 
+           ,url: "/aj/cloud_comments"
+           ,data: data
+           ,success:function(res){
+               resolve(res);
+           }
+           ,error:function(err){
+               reject(err);
+           }
+        });
+    });
 
+    const delay = (duration) => new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve();
+        }, duration);
+    });
+
+    const makeCloud = (videoID, videoTime, hash, asciiArt) => {
+        console.log(`${videoID} / ${videoTime} / ${hash}`);
+
+        const asciiRows = asciiArt.split('\n');
+        console.log(asciiRows);
+        asciiRows.forEach(async (asciiCols, row, array) => {
+            for(var i = 0; i < asciiCols.length; i ++) {
+                try {
+                    const result = await reqCloud({
+                            video_id: videoID,
+                            text: asciiCols.charAt(i),
+                            location_x: i,
+                            location_y: row,
+                            currentTime: videoTime,
+                            hash: hash
+                        });
+                    console.log(`${row}, ${i} : ${JSON.stringify(result)}`);
+                    await delay(1000);
+                }
+                catch (e) {
+                    console.error(`${row}, ${i} : ${JSON.stringify(e)}`);
+                }
+            }
+        })
     };
     
     $("#dropbox").on("dragover", (ev) => {
@@ -122,6 +164,11 @@ javascript: (function () {
                     const ascii = drawAscii(grayScales, width);
                     canvas.width = 0;
                     canvas.height = 0;
+
+                    makeCloud($('#video-id').val(), 
+                        document.querySelector("#my-video_from_mejs").player.currentTime, 
+                        $('.main_session').val(),
+                        ascii);
                 };
                 image.src = event.target.result;
             };
@@ -132,3 +179,38 @@ javascript: (function () {
 
 
 
+/*
+
+            for(var i = 0; i < asciiCols.length; i ++) {
+                try {
+                    const result = await reqCloud({
+                            video_id: videoID,
+                            text: asciiCols.charAt(i),
+                            location_x: i,
+                            location_y: row,
+                            currentTime: videoTime,
+                            hash: hash
+                        });
+                    console.log(`${row}, ${i} : ${result}`);
+                }
+                catch (e) {
+                    console.error(`${row}, ${i} : ${e}`);
+                }
+
+
+
+            try {
+                const result = await reqCloud({
+                        video_id: videoID,
+                        text: asciiCols,
+                        location_x: 0,
+                        location_y: row,
+                        currentTime: videoTime,
+                        hash: hash
+                    });
+                console.log(`0, ${row} : ${JSON.stringify(result)}`);
+            }
+            catch (e) {
+                console.error(`0, ${row} : ${JSON.stringify(e)}`);
+            }
+            }*/
